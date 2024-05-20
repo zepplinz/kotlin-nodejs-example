ac1243f066d63398f1f935e7ffe1bcdaf1a52895
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-05-20 21:43:33 +0000

    edit_file

--- backend-js/src/main/kotlin/pappel/fetch.kt
+++ backend-js/src/main/kotlin/pappel/fetch.kt
@@ -1,17 +1,7 @@
-package pappel
-
-import org.w3c.fetch.RequestInit
-import org.w3c.fetch.Response
-import kotlin.js.Promise
-
-fun fetch(input: dynamic): Promise<Response> {
-    return Fetch.jsFetch(input) as Promise<Response>
+import fetch, { RequestInit, Response } from 'node-fetch';
+function fetch(input: any): Promise<Response> {
+    return fetch(input) as Promise<Response>;
 }
-
-fun fetch(input: dynamic, init: RequestInit): Promise<Response> {
-    return Fetch.jsFetch(input, init.asDynamic())
-}
-
-object Fetch {
-    val jsFetch: dynamic = require("node-fetch")
+function fetch(input: any, init: RequestInit): Promise<Response> {
+    return fetch(input, init);
 }
