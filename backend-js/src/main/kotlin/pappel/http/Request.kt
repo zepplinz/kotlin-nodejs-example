2dfa30b9565fa3b45104c443867101a3ed73be9d
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-05-20 21:44:00 +0000

    edit_file

--- backend-js/src/main/kotlin/pappel/http/Request.kt
+++ backend-js/src/main/kotlin/pappel/http/Request.kt
@@ -2,38 +2,57 @@
  * Adapted from Raphael Stäbler's Pappel Node.js framework for Kotlin
  * https://github.com/blazer82/pappel-framework
  */
-
-package pappel.http
-
-import pappel.JSONUtils
-
-class Request(external private val req: dynamic) {
-
-    val baseURL: String
-    val body: Map<String, Any>?
-    val cookies: Map<String, String>?
-    val hostname: String
-    val ip: String
-    val ips: Array<String>?
-    val method: Method
-    val parameters: Map<String, String>?
-    val path: String
-    val protocol: Protocol
-    val query: Map<String, Any>?
-    // TODO: Add route
-
-    init {
-        baseURL = req.baseUrl as String
-        body = null
-        cookies = null
-        hostname = req.hostname as String
-        ip = req.ip as String
-        ips = req.ips as? Array<String>
-        method = Method.valueOf(req.method as String)
-        parameters = JSONUtils.retrieveMap(req.params) as? Map<String, String>
-        path = req.path as String
-        protocol = Protocol.valueOf((req.protocol as String).toUpperCase())
-        query = JSONUtils.retrieveMap(req.query) as? Map<String, Any>
-    }
-
+import { JSONUtils } from './JSONUtils';
+enum Method {
+    GET = 'GET',
+    POST = 'POST',
+    PUT = 'PUT',
+    DELETE = 'DELETE',
+    PATCH = 'PATCH',
+    OPTIONS = 'OPTIONS',
+    HEAD = 'HEAD'
 }
+enum Protocol {
+    HTTP = 'HTTP',
+    HTTPS = 'HTTPS'
+}
+interface Request {
+    baseURL: string;
+    body: { [key: string]: any } | null;
+    cookies: { [key: string]: string } | null;
+    hostname: string;
+    ip: string;
+    ips: string[] | null;
+    method: Method;
+    parameters: { [key: string]: string } | null;
+    path: string;
+    protocol: Protocol;
+    query: { [key: string]: any } | null;
+}
+class RequestImpl implements Request {
+    baseURL: string;
+    body: { [key: string]: any } | null;
+    cookies: { [key: string]: string } | null;
+    hostname: string;
+    ip: string;
+    ips: string[] | null;
+    method: Method;
+    parameters: { [key: string]: string } | null;
+    path: string;
+    protocol: Protocol;
+    query: { [key: string]: any } | null;
+    constructor(private req: any) {
+        this.baseURL = req.baseUrl as string;
+        this.body = null;
+        this.cookies = null;
+        this.hostname = req.hostname as string;
+        this.ip = req.ip as string;
+        this.ips = req.ips as string[] | null;
+        this.method = Method[req.method as keyof typeof Method];
+        this.parameters = JSONUtils.retrieveMap(req.params) as { [key: string]: string } | null;
+        this.path = req.path as string;
+        this.protocol = Protocol[(req.protocol as string).toUpperCase() as keyof typeof Protocol];
+        this.query = JSONUtils.retrieveMap(req.query) as { [key: string]: any } | null;
+    }
+}
+export { Request, RequestImpl, Method, Protocol };
